5d13e2672d8be5f5e26d23ab2473e3cb436a1cfb
MARTEL, Loïc <loic.martel@cgi.com>
2021-04-21 17:43:08 +0200

    fix r_type in read_sensor_file

--- src/functions.rs
+++ src/functions.rs
@@ -1,17 +1,22 @@
 use std::path;
 use std::fs;
 use std::io;
+use std::error::Error;
+use std::str::FromStr;
 
 
 // try to read the sensor file.
-fn read_sensor_file(sensor_file: &std::path::PathBuf) -> Result<String, io::Error> {
-    fs::read_to_string(sensor_file)
+fn read_sensor_file(sensor_file: &std::path::PathBuf) -> Result<String, String> {
+    match fs::read_to_string(sensor_file){
+        Ok(f_content) => Ok(f_content),
+        Err(m_error) => panic!("Files doest not exists!")
+    }
 }
 
 // Extract temperature from a string object.
-fn extract_temperature(sensor_content: &String) -> Result<f32, io::Error> {
-    let string_temp = sensor_content.lines().nth(2)?.split(" ").nth(9)?;
-    f32::from(string_temp);
+fn extract_temperature(sensor_content: &String) -> Result<f32, std::num::ParseFloatError> {
+    let mut string_temp = sensor_content.lines().nth(2).unwrap().split(" ").nth(9).unwrap();
+    f32::from_str(string_temp)
 }
 
 pub fn get_temperature(sensor_file: &std::path::PathBuf) -> Result<f32, Box<dyn Error>> {
@@ -25,7 +30,6 @@ pub fn get_temperature(sensor_file: &std::path::PathBuf) -> Result<f32, Box<dyn 
     // donnees_temperature = seconde_ligne.split(" ")[9]
     // # Supprimer le "t=", et ajouter une virgule
     // return float(donnees_temperature[2:]) / 1000
-    Ok(())
 }
 
 
