1c75a6f68380d9634c2a09dee822767dc1afc683
loic <loic@localhost.localdomain>
2021-04-24 18:41:07 +0200

    can extract temp well

--- src/functions.rs
+++ src/functions.rs
@@ -1,23 +1,31 @@
-use std::path;
+use std::error::Error;
 use std::fs;
 use std::io;
-use std::error::Error;
+use std::path;
 use std::str::FromStr;
-
 
 // try to read the sensor file.
 fn read_sensor_file(sensor_file: &std::path::PathBuf) -> Result<String, String> {
-    match fs::read_to_string(sensor_file){
+    match fs::read_to_string(sensor_file) {
         Ok(f_content) => Ok(f_content),
-        Err(m_error) => panic!("Files doest not exists!")
+        Err(m_error) => panic!("Files doest not exists!"),
     }
 }
 
 // Extract temperature from a string object.
 fn extract_temperature(sensor_content: &String) -> Result<f32, std::num::ParseFloatError> {
     println!("sensor file content: \n {}", sensor_content);
-    let string_temp = sensor_content.lines().nth(2).unwrap().split(" ").nth(9).unwrap();
-    f32::from_str(string_temp)
+    let mut string_temp = sensor_content
+        .lines()
+        .nth(1)
+        .unwrap()
+        .split(" ")
+        .nth(9)
+        .unwrap();
+    string_temp = &string_temp[2..];
+    println!("str_temp is {}", string_temp);
+    let res = f32::from_str(string_temp).unwrap() / 1000f32;
+    Ok(res)
 }
 
 pub fn get_temperature(sensor_file: &std::path::PathBuf) -> f32 {
@@ -31,17 +39,18 @@ pub fn get_temperature(sensor_file: &std::path::PathBuf) -> f32 {
     // return float(donnees_temperature[2:]) / 1000
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_extracting() {
-        let contents = String::from_str("\
+        let contents = String::from_str(
+            "\
 6E 50 f4 20 f5 05 J7 9U 9H : crc=90 YES
-6E 50 f4 20 f5 05 J7 9U 9H t=6787").unwrap();
-        assert_eq!(6.87f32, extract_temperature(&contents).unwrap())
+6E 50 f4 20 f5 05 J7 9U 9H t=6787",
+        )
+        .unwrap();
+        assert_eq!(6.787f32, extract_temperature(&contents).unwrap())
     }
-
 }
